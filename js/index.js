401da0b351eddb8857767fee9c72a05d7d17cac3
OlukaDenis <olukadeno@gmail.com>
2020-04-07 23:52:28 +0300

    Fixed linter errors

--- js/index.js
+++ js/index.js
@@ -14,17 +14,16 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary('Book 1', 'Denis', 800, 0);
 addBookToLibrary('Book 2', 'Josue', 500, 0);
 
-function displayAlert(message, messageType){
-  console.log('Alert is opened')
+function displayAlert(message, messageType) {
   const alert = document.querySelector('.alert');
   const alertMessage = document.querySelector('#message');
   const alertType = document.querySelector('#message-type');
   alert.style.display = 'block';
   alertMessage.innerHTML = message;
   alertType.innerHTML = messageType;
-  if (messageType === 'Danger!'){
+  if (messageType === 'Danger!') {
     alert.style.backgroundColor = '#f44336';
-  } else if (messageType === 'Success!'){
+  } else if (messageType === 'Success!') {
     alert.style.backgroundColor = '#00802b';
   } else {
     alert.style.backgroundColor = '#ffa31a';
@@ -34,10 +33,10 @@ function displayAlert(message, messageType){
 function render() {
   const container = document.querySelector('.items-container');
   container.innerHTML = '';
-  for (let i = 0; i < myLibrary.length; i = i + 1) {
+  for (let i = 0; i < myLibrary.length; i++) {
     const obj = myLibrary[i];
     container.innerHTML
-    +=`
+    += `
       <div class='item card'> 
           <p class='title'><span>Title: </span>${obj.title}</p>
           <p class='pages'>${obj.pages}<span> Pages</span></p>
@@ -69,7 +68,7 @@ function addFormValues() {
     const author = document.getElementById('author');
     const read = document.getElementById('read').checked ? 1 : 0;
     const pages = document.getElementById('pages');
-    if (title.value === null || title.value === '' ) {
+    if (title.value === null || title.value === '') {
       displayAlert('Title must not be empty.', 'Danger!');
     } else if (author.value === null || author.value === '') {
       displayAlert('Author must not be empty.', 'Danger!');
